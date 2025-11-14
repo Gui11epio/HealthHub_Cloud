ebaeab00d80ac822b8207420e6a9e9cd18dc1eba
Guilherme Laiber <162480837+Gui11epio@users.noreply.github.com>
2025-11-14 08:44:19 -0300

    Enhance QuestionarioController with Swagger annotations

--- Health-Hub/Controllers/QuestionarioController.cs
+++ Health-Hub/Controllers/QuestionarioController.cs
@@ -1,4 +1,4 @@
-﻿using Health_Hub.Application.DTOs.Request;
+using Health_Hub.Application.DTOs.Request;
 using Health_Hub.Application.DTOs.Response;
 using Health_Hub.Application.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -20,6 +20,13 @@ namespace Health_Hub.Controllers
         }
 
         [HttpGet("{id}")]
+        [ProducesResponseType(typeof(QuestionarioResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [SwaggerOperation(
+            Summary = "Obtém um questionário por ID",
+            Description = "Retorna os detalhes de um questionário específico"
+        )]
         public async Task<IActionResult> GetById(int id)
         {
             var questionario = await _svc.ObterPorId(id);
@@ -28,6 +35,12 @@ namespace Health_Hub.Controllers
         }
 
         [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<QuestionarioResponse>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [SwaggerOperation(
+            Summary = "Obtém todos os questionários",
+            Description = "Retorna uma lista de todos os questionários cadastrados."
+        )]
         public async Task<IActionResult> GetAll()
         {
             var questionario = await _svc.ObterTodos();
@@ -36,6 +49,12 @@ namespace Health_Hub.Controllers
         }
 
         [HttpGet("pagina")]
+        [ProducesResponseType(typeof(PagedResult<QuestionarioResponse>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [SwaggerOperation(
+            Summary = "Obtém questionários paginados",
+            Description = "Retorna uma lista paginada de questionários."
+        )]
         public async Task<ActionResult<PagedResult<QuestionarioResponse>>> GetPaged(int numeroPag = 1, int tamanhoPag = 10)
         {
             var result = await _svc.ObterPorPagina(numeroPag, tamanhoPag);
@@ -43,6 +62,8 @@ namespace Health_Hub.Controllers
         }
 
         [HttpPost]
+        [ProducesResponseType(typeof(QuestionarioResponse), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [SwaggerOperation(
             Summary = "Cria um novo questionário e gera uma análise de saúde mental.",
             Description = "Com base nos níveis de estresse, sono, ansiedade e sobrecarga informados, retorna um resumo sobre o bem-estar mental do colaborador."
@@ -60,6 +81,12 @@ namespace Health_Hub.Controllers
         }
 
         [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [SwaggerOperation(
+            Summary = "Remove um questionário",
+            Description = "Remove um questionário específico do sistema."
+        )]
         public async Task<IActionResult> Delete(int id)
         {
             var deleted = await _svc.Deletar(id);
